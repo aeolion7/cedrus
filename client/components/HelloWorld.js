d31fab74fba8a63c15ca5816213b23b65aa99459
Joseph Lionarons <lionsamurai@gmail.com>
2019-04-02 14:34:17 -0400

    added central react component export file

---
+++ client/components/HelloWorld.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class HelloWorld extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <div>Hello, world!</div>;
+  }
+}
---
+++ client/components/index.js
@@ -0,0 +1,7 @@
+/*
+  This is a central component export file. This allows all components to be
+  imported from this file, rather than having to import each component
+  from its own file.
+*/
+
+export { default as HelloWorld } from './HelloWorld';
