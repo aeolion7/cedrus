969a4648c0018323e6c58bda33b3eab8daebf234
Joseph Lionarons <lionsamurai@gmail.com>
2019-04-02 17:23:26 -0400

    added redux

--- client/app.js
+++ client/app.js
@@ -1,5 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './store/store';
 import { HelloWorld } from './components/index';
 
-ReactDOM.render(<HelloWorld />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <HelloWorld />
+  </Provider>,
+  document.getElementById('root')
+);
