06509e76444c42f900b8d3b24278a33dd3719f41
Joseph Lionarons <lionsamurai@gmail.com>
2019-04-03 11:03:38 -0400

    structured express server

---
+++ server/api/index.js
@@ -0,0 +1,15 @@
+// Central API route handler.
+
+const router = require('express').Router();
+
+// Further API route handling can be done here, for example:
+// router.use('/users', require('./users'));
+
+// Handler for requests that do not match
+router.use((req, res, next) => {
+  const err = new Error('404 Not Found.');
+  err.status = 404;
+  next(err);
+});
+
+module.exports = router;
--- server/app.js
+++ server/app.js
@@ -1,13 +1,26 @@
 const express = require('express');
+const app = express();
 const path = require('path');
+const volleyball = require('volleyball');
+const bodyParser = require('body-parser');
 const PORT = 3000;
 
-const app = express();
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '..', 'public')));
+app.use(volleyball);
+
+app.use('/api', require('./api'));
 
 app.get('*', (req, res, next) => {
-  res.send('index.html');
+  res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+});
+
+// Error handler
+app.use((err, req, res, next) => {
+  console.error(err);
+  console.error(err.stack);
+  res.send(err.status || 500).send(err.message || 'Internal Server Error.');
 });
 
 app.listen(PORT, () => {
